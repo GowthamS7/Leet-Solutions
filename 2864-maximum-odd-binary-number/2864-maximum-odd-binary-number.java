a77a84e0ea276c62d2d055d5f8f1b2a5ed324da2
GowthamGT <133477253+GowthamS7@users.noreply.github.com>
2024-03-01 09:37:15 +0530

    Time: 1 ms (100%), Space: 43.3 MB (58.27%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -4,17 +4,11 @@ class Solution {
         int c=0;
         for(int i=0; i<n; i++){
             if(s.charAt(i) == '1'){
-                c++;
-            }
-        }
+                c++;}}
         StringBuilder r = new StringBuilder();
         for(int i=0; i<n; i++){
-            r.append('0');
-        }
+            r.append('0');}
         r.setCharAt(n-1, '1');
         for(int i=0; i<c-1; i++){
-            r.setCharAt(i, '1');
-        }
-        return r.toString();
-    }
-}
+            r.setCharAt(i, '1');}
+        return r.toString();}}
