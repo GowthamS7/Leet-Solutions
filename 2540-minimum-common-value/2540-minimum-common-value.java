6fb848ebf392c50080a57a40887f5d434f8637ca
GowthamGT <133477253+GowthamS7@users.noreply.github.com>
2024-03-09 12:44:00 +0530

    Time: 20 ms (7.26%), Space: 65.1 MB (5.63%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,14 +1,8 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int num : nums1){
-            map.put(num, map.getOrDefault(num, 0)+1);
-        }
-        for(int num : nums2){
-            if(map.containsKey(num) && map.get(num) > 0){
-                return num;
-            }
-        }
+        for(int num : nums1){map.put(num, map.getOrDefault(num, 0)+1);}
+        for(int num : nums2){if(map.containsKey(num) && map.get(num) > 0){return num;}}
         return -1;
     }
 }
